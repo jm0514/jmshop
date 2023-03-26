70db2df8559b70b373dad11c1f4dfa541af0616a
JeongMin <skawjdals96@gmail.com>
2023-03-26 21:53:39 +0900

    Feat: Item 엔티티 추가

--- src/main/java/com/shop/jmshop/entity/Item.java
+++ src/main/java/com/shop/jmshop/entity/Item.java
@@ -1,4 +1,30 @@
 package com.shop.jmshop.entity;
 
+import com.shop.jmshop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
 public class Item {
+
+    private Long id;
+
+    private String itemNm;
+
+    private int price;
+
+    private int stockNumber;
+
+    private String itemDetail;
+
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
 }
