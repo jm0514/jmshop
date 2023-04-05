6bf430357c2ef2508ef5ab7d6123428f47692589
JeongMin <skawjdals96@gmail.com>
2023-04-05 18:05:43 +0900

    Feat: Controller 생성
    메인 화면, 상품 목록, 회원 가입, 로그인 구현

---
+++ src/main/java/com/shop/jmshop/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.shop.jmshop.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
