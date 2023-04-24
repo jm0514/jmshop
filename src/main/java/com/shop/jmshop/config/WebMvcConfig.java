a0bb28ce110ded51e992bb738f8984db85a59fe5
JeongMin <skawjdals96@gmail.com>
2023-04-24 15:17:14 +0900

    Comment: 정적 파일의 경로를 설정하는 WebMvcConfigurer에 대한 설명

--- src/main/java/com/shop/jmshop/config/WebMvcConfig.java
+++ src/main/java/com/shop/jmshop/config/WebMvcConfig.java
@@ -7,10 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
+    //Spring MVC에서 정적 파일의 경로를 설정하는 코드
     @Value("${uploadPath}")
     String uploadPath;
 
+    //URL이 "/images/" 패턴으로 요청이 들어올 경우 uploadPath의 경로를 기준으로 정적 파일을 찾아서 응답
+    //ItemImgService에서 imgUrl을 "/images/item" + 이미지 이름으로 지정했기 때문에 "D:/study/shop/item" 경로에서 파일을 찾음
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
