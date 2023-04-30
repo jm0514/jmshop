bfa6ddd9333b4fd32c68789eb5fb18911c7e4d0a
JeongMin <skawjdals96@gmail.com>
2023-04-30 23:52:59 +0900

    Feat: 장바구니, 상품, 개수를 통해 장바구니 상품 목록을 생성하는 메서드 추가
    개수를 추가하고 개수를 갱신하는 메서드도 추가

--- src/main/java/com/shop/jmshop/entity/CartItem.java
+++ src/main/java/com/shop/jmshop/entity/CartItem.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "cart_item")
-public class CartItem {
+public class CartItem extends BaseEntity{
 
     @Id
     @GeneratedValue
@@ -26,4 +26,22 @@ public class CartItem {
     private Item item;
 
     private int count;
+
+    public static CartItem createCartItem(Cart cart, Item item, int count) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count) {
+        this.count += count;
+    }
+
+    public void updateCount(int count) {
+        this.count = count;
+    }
+
+
 }
