e379488809adfaf0ee2d5db7964e2f79b1e7fb2c
JeongMin <skawjdals96@gmail.com>
2023-04-30 23:58:09 +0900

    Feat: 회원 id를 통해 장바구니를 조회하는 인터페이스 추가

--- src/main/java/com/shop/jmshop/repository/CartRepository.java
+++ src/main/java/com/shop/jmshop/repository/CartRepository.java
@@ -1,8 +1,9 @@
 package com.shop.jmshop.repository;
 
-
 import com.shop.jmshop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId);
 }
